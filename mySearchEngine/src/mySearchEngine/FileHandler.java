dbc93060c5e1ad5e9b7aae55699add71be9c545f
Max Blennerhassett <C16704549@mydit.ie>
2019-04-09 17:21:57 +0100

    Update FileHandler.java

--- mySearchEngine/src/mySearchEngine/FileHandler.java
+++ mySearchEngine/src/mySearchEngine/FileHandler.java
@@ -13,49 +13,6 @@ import java.util.Scanner;
 public class FileHandler 
 {
 
-	String fileName; // To store the file name in
-	File myFile; // File type
-	Scanner myScanner;
 
-	// Constructor
-	FileHandler (String fileName)
-	{
-		
-		this.fileName = fileName;
-		
-	}
-	
-	// Connection
-	void establishConnection()
-	{
-		myFile = new File(fileName);
-	}
-	
-	// Read and return (test)
-	String [] readFile()
-	{
-		
-		String[] values = new String[6];
-	    try
-		{
-	    	int i = 0;
-	    	myScanner = new Scanner(myFile); 
-			 while (myScanner.hasNextLine())
-			    {
-			      
-				 values[i] = myScanner.nextLine();
-			      i++;
-			    }
-		}
-		catch (FileNotFoundException e)
-		{
-			System.out.println("run time error " + e.getMessage());
-		}
-	    finally
-	    {
-	        return values;
-	    }
-		
-	}
 
 }
